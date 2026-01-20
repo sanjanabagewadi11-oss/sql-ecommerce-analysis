abbc4bffa395435b6be85e37dd2a90c1322d454b
sanjanabagewadi11-oss <sanjanabagewadi11@gmail.com>
2026-01-20 14:40:47 +0530

    Update 01_schema.sql

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,1 +1,33 @@
+CREATE TABLE orders (
+    order_id VARCHAR(100) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_purchase DATETIME,
+    order_status VARCHAR(50)
+);
+SELECT COUNT(*) FROM ORDERS;
+
+CREATE TABLE customers(
+customer_id VARCHAR(100),
+customer_city VARCHAR(50),
+customer_state VARCHAR(50)
+);
+SELECT COUNT(*) FROM customers;
+
+CREATE TABLE products(
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_weight INT,
+    product_length INT,
+    product_height INT,
+    product_width INT
+);
+SELECT COUNT(*) FROM products;
+
+CREATE TABLE order_items (
+    order_id VARCHAR(50),
+    product_id VARCHAR(50),
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2)
+);
+SELECT COUNT(*) FROM order_items;
 
