4b9f5919e3419b83a0be73a8e0051b1ec959134b
sanjanabagewadi11-oss <sanjanabagewadi11@gmail.com>
2026-01-20 14:51:42 +0530

    Update 05_revenue_analysis.sql

--- sql/sql/05_revenue_analysis.sql
+++ sql/sql/05_revenue_analysis.sql
@@ -1,1 +1,9 @@
-
+SELECT 
+    product_category_name,
+    product_id,
+    SUM(price) AS revenue
+FROM order_items 
+JOIN products USING(product_id)
+  GROUP BY product_category_name, product_id
+ORDER BY revenue DESC
+LIMIT 5;
