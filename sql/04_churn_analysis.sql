eb65f2bf7bc5a9593fc2fc444a646585109abd16
sanjanabagewadi11-oss <sanjanabagewadi11@gmail.com>
2026-01-20 14:49:31 +0530

    Update 04_churn_analysis.sql

--- sql/04_churn_analysis.sql
+++ sql/04_churn_analysis.sql
@@ -1,1 +1,15 @@
-
+WITH last_order AS (
+  SELECT customer_id, MAX(order_date) last_order_date
+  FROM orders
+  GROUP BY customer_id
+)
+SELECT
+  CASE
+    WHEN DATEDIFF('2022-01-01', last_order_date) > 90
+         THEN 'Churned'
+    ELSE 'Active'
+  END AS customer_status,
+  COUNT(*) customers
+FROM customers c
+JOIN last_order ON c.customer_id = last_order.customer_id
+GROUP BY 1;
