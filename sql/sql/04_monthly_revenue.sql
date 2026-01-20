1ba15a71789b96816dee78e0aa9c743e87d35894
sanjanabagewadi11-oss <sanjanabagewadi11@gmail.com>
2026-01-20 14:54:18 +0530

    Update 04_monthly_revenue.sql

--- sql/sql/04_monthly_revenue.sql
+++ sql/sql/04_monthly_revenue.sql
@@ -1,1 +1,17 @@
-
+WITH monthly_sales AS (
+    SELECT
+        DATE_FORMAT(order_date, '%Y-%m-01') AS month,
+        SUM(price) AS revenue
+    FROM orders join order_items using(order_id)
+    GROUP BY DATE_FORMAT(order_date, '%Y-%m-01')
+)
+SELECT
+    month,
+    revenue,
+    LAG(revenue) OVER (ORDER BY month) AS prev_month,
+    ROUND(
+        (revenue - LAG(revenue) OVER (ORDER BY month)) 
+        / LAG(revenue) OVER (ORDER BY month) * 100,
+    2) AS growth_pct
+FROM monthly_sales
+ORDER BY month;
